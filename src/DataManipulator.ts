f8b03cbc28f899faa251259b8a22c7a560ca7c6b
SYED MOHAMMAD AQUIB <syedmohammadaquib12@gmail.com>
2024-06-17 21:41:02 +0530

    RESOLVED

--- src/DataManipulator.ts
+++ src/DataManipulator.ts
@@ -31,22 +31,3 @@ export class DataManipulator {
   
   }
 }
-
-export class DataStreamer {
-  static API_URL: string = 'http://localhost:3000/query?id=1';
-
-  static getData(callback: (data: ServerRespond[]) => void): void {
-    const request = new XMLHttpRequest();
-    request.open('GET', DataStreamer.API_URL, false);
-
-    request.onload = () => {
-      if (request.status === 200) {
-        callback(JSON.parse(request.responseText));
-      } else {
-        alert('Request failed');
-      }
-    };
-
-    request.send();
-  }
-}
